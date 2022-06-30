6ce4884a0bf69800554ecd318eb6d3ca6f605c0d
Matthew Vincent <mvincent@basecamp.com>
2022-06-30 15:28:04 +0300

    enabling new loci and editing from web app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_db, :set_descriptions
-  http_basic_authenticate_with name: "balua", password: "k3r@k2019"
+  http_basic_authenticate_with name: "balua", password: "k3r@k2019" if Rails.env.production?
 
   private
   def set_db
