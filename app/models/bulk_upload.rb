45e3153e31409826c4f00ca6ad1e73cc4decd45d
Matthew Vincent <matt@averails.com>
2023-06-12 00:04:09 +0100

    fixing bulk upload model

--- app/models/bulk_upload.rb
+++ app/models/bulk_upload.rb
@@ -1,22 +1,3 @@
-class BulkUploadsController < ApplicationController
-  def new
-  end
+class BulkUpload
 
-  def create
-    uploaded_files = params[:files]
-    uploaded_file_keys = []
-
-    uploaded_files.each do |file|
-      s3_object = Aws::S3::Resource.new.bucket('your_bucket_name').object(file.original_filename)
-
-      s3_object.upload_file(file.tempfile.path, acl: 'public-read') do |progress|
-        # Progress tracking logic here
-        puts "Uploaded #{progress.loaded} of #{progress.total} bytes"
-      end
-
-      uploaded_file_keys << s3_object.key
-    end
-
-    render json: { keys: uploaded_file_keys }
-  end
 end
